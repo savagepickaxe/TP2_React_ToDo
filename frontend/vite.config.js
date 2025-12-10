40efa43e792c6d3325d96c420109c3eb82fa79cb
unknown <assetwarmup@yahoo.com>
2025-12-09 21:37:36 -0500

    ajout tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { fileURLToPath, URL } from 'node:url'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+      tailwindcss(),
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
