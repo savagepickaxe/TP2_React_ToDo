3ecdafeefee73a73a0ec78f7da56e208dd681628
unknown <assetwarmup@yahoo.com>
2025-12-10 00:17:45 -0500

    taiolwind css

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,12 +1,35 @@
 export default function Button(props) {
-  return (
+  let background;
+ let color;
+ let rounded = 'rounded-lg'; ;
+  switch (props.type) {
+    case 'primary':;
+  background = 'bg-gray-300';
+      break;
+    case 'modifier':
+      background = 'bg-yellow-300';
+      break;
+    case 'supprimer':
+      background = 'bg-red-500';
+      color = 'text-white';
+      break;  
+    case 'enregister':
+      background = 'bg-blue-500';
+      color = 'text-white';
+      rounded = 'rounded-[0.5vw]';
+      break;
+      
+    default:
+        color = 'text-black';   
+        rounded = 'rounded-lg';
+      break;
+  }
+     return (
     <button
-        type={props.type ? props.type : "button"}
-        className={`todo-item__button ${props.className ? props.className : ""}`}
-        id={props.id ? props.id : undefined}
+        className={`p-2 px-5 ${rounded} shadow-md todo-item ${background} ${color}`}
         onClick={props.onClick ? props.onClick : undefined}
     >
       {props.children}
-    </button>
-  );
+    </button>     
+ );
 }
